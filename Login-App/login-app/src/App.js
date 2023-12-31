75b6c8eae0a12a2f329edabf330822974ea82b54
neha7227 <neha.indoliya27@gmail.com>
2023-12-31 16:29:33 +0530

    welcome msg

--- Login-App/login-app/src/App.js
+++ Login-App/login-app/src/App.js
@@ -1,51 +1,68 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [obj, setObj]= useState({})
-  const [invalidMsg, setInvalidMsg]= useState(false);
-  const [welcomeMsg, setWelcomeMsg]= useState(false)
-  const handleSubmit=(e)=>{
-e.preventDefault()
-if(obj.username === "user" && obj.password==="password") setWelcomeMsg(true)
-// {const welcome = "Welcome, user!";
+  const [obj, setObj] = useState({});
+  const [invalidMsg, setInvalidMsg] = useState(false);
+  const [welcomeMsg, setWelcomeMsg] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.username === "user" && obj.password === "password")
+      setWelcomeMsg(true);
+    // {const welcome = "Welcome, user!";
+    // else { const invalid = "Invalid username or password"
+    else setInvalidMsg(true);
+  };
 
-// else { const invalid = "Invalid username or password"
-else setInvalidMsg(true)
-  }
+  const handleInput = (e) => {
+    const { name, value } = e.target; // destructuring the values from e event
+    // console.log(e.target.type, "e.target")
+    const newObj = { ...obj, [`${name}`]: value };
+    // console.log(newObj, "before change")
 
+    setObj(newObj);
+    // console.log(newObj, "after change")
+  };
 
-
-  const handleInput=(e)=>{
-const {name,value}=e.target; // destructuring the values from e event
-// console.log(e.target.type, "e.target")
-const newObj = {...obj, [`${name}`]:value};
-// console.log(newObj, "before change")
-
-setObj(newObj);
-// console.log(newObj, "after change")
-  }
-
- return (
+  return (
     <div className="App">
-{(welcomeMsg)?<p>Welcome, {obj.username}!</p>:
-(<form className="form-container" onSubmit={handleSubmit}>
-<h1>Login Page</h1>
-{(invalidMsg)?<p>Invalid username or password</p>:""}
-  <label>Username:<input id="username" name="username" type="text" placeholder="username" required onChange={handleInput}></input></label>
+      <h1>Login Page</h1>
+      {welcomeMsg ? (
+        <p>Welcome, {obj.username}!</p>
+      ) : (
+        <form className="form-container" onSubmit={handleSubmit}>
 
+          {invalidMsg ? <p>Invalid username or password</p> : ""}
+          <label>
+            Username:
+            <input
+              id="username"
+              name="username"
+              type="text"
+              placeholder="username"
+              required
+              onChange={handleInput}
+            ></input>
+          </label>
 
-  <label>Password:<input id="password" name="password" type="password" placeholder="password" required onChange={handleInput}></input></label>
+          <label>
+            Password:
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="password"
+              required
+              onChange={handleInput}
+            ></input>
+          </label>
 
-
-
-<div className="submitBtn">
-{/* <button type="submit" onClick={handleSubmit}>Submit</button> */}
-<button type="submit">Submit</button>
-
-</div>
-</form>)
-}
+          <div className="submitBtn">
+            {/* <button type="submit" onClick={handleSubmit}>Submit</button> */}
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
